fe61921526d547a425c3b4a650eb3b028ede5b5c
jm33-m0 <jm33.m0@gmail.com>
2020-11-22 15:30:29 +0800

    add ps script

---
+++ mouse-natural-scroll.ps1
@@ -0,0 +1,28 @@
+Write-Host "Finding your USB mouse..." -ForegroundColor Cyan
+
+$usb_mouse = Get-CimInstance -ClassName  Win32_PointingDevice | Where-Object Name -Match "USB Input Device" | Select-Object -ExpandProperty DeviceID
+if (!$usb_mouse) {
+    Write-Host -ForegroundColor Red "Could not find your USB mouse"
+    exit 1
+}
+$id = $usb_mouse.split("\\")[1]
+$device = Get-PnpDevice -PresentOnly -Class Mouse | Where-Object DeviceID -Match $id 
+$name = $device | Select-Object -ExpandProperty FriendlyName
+$path = $device | Select-Object -ExpandProperty InstanceId
+
+Write-Host "Found $name ($path)" -ForegroundColor Green 
+Write-Host "Configuring natural scrolling feature for your mouse..." -ForegroundColor Cyan
+
+$currentPrincipal = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
+if (!$currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+    Write-Host -ForegroundColor Red "Need admin privilege to modify the registry"
+    exit 1
+}
+
+if ($args[0] -eq "reverse") {
+    Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Enum\$path\Device Parameters" -Name FlipFlopWheel -Value 0
+    Write-Host "Natural scrolling feature for your mouse has been DISABLED" -ForegroundColor Green
+    exit
+}
+Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Enum\$path\Device Parameters" -Name FlipFlopWheel -Value 1
+Write-Host "Natural scrolling feature for your mouse has been ENABLED" -ForegroundColor Green
